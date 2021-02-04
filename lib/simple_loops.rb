90e05b3836ef84f4c5281bbb32d4b0960c692413
Barak Rosner <rosnerbb@gmail.com>
2021-02-04 23:48:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,5 @@
 def loop_message_five_times(message)
     count = 0 
-    
     while count < 5 do
       puts "Hello world" 
       count += 1
@@ -10,7 +9,6 @@ end
 
 def loop_message_n_times (message, limit)
     count = 0 
-    
     while count < limit do 
       puts "Hello Moon" 
       count += 1
@@ -20,10 +18,10 @@ end
 
 def output_array (array)
     count = 0 
-    
     while count < array.length do
       puts array [count]
       count += 1 
+    end 
 end 
 
 
